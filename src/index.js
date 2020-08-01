a44c9692465371dd4844d99b7616f92a06e79403
shotasan <wondermoai@gmail.com>
2020-08-01 23:10:46 +0900

    implement simple html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function compopnent() {
+  const element = document.createElement('div');
+  const array = ['Hello', 'webpack'];
+  element.innerHTML = _.join(array, ' ');
+  return element;
+}
+
+document.body.appendChild(compopnent());
